8cf43f780e29a06cdd9259598a893cf10c49610d
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-08 00:42:46 -0600

    artwork
    ASCII art for the program

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from art import logo
+
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -8,37 +12,30 @@ shift = int(input("Type the shift number:\n"))
 # index = alphabet.index('d')
 # print(index)
 
-def encrypt(text, shift):
+def caesar(text, shift,direction):
     text_list = list(text)
     i = 0
     for char in text_list:
-        index = alphabet.index(char)
-        shifted_index = index + shift
-        if shifted_index > 25:
-            index = shifted_index - 26
-            text_list[i] = alphabet[index]
+        if char in alphabet:
+            index = alphabet.index(char)
+            if direction == 'encode':
+                shifted_index = index + shift
+            elif direction == 'decode':
+                shifted_index = index - shift
+                
+            if shifted_index > 25 and direction == 'encode':
+                index = shifted_index - 26
+                text_list[i] = alphabet[index]
+            elif shifted_index < 0 and direction == 'decode':
+                index = shifted_index + 26
+                text_list[i] = alphabet[index]
+            else:
+                text_list[i] = alphabet[shifted_index]
+            i += 1
         else:
-            text_list[i] = alphabet[index + shift]
-        i += 1
+            text_list[i] = char
+            i +=1
     text_list = ''.join(text_list)      
-    print(text_list)
+    print(f"Here's the {direction}d result: {text_list}")
 
-def decrypt(text, shift):
-    text_list = list(text)
-    i = 0
-    for char in text_list:
-        index = alphabet.index(char)
-        shifted_index = index - shift
-        if shifted_index < 0:
-            index = shifted_index + 26
-            text_list[i] = alphabet[index]
-        else:
-            text_list[i] = alphabet[shifted_index]
-        i += 1
-    text_list = ''.join(text_list)      
-    print(text_list)
-
-if direction == 'encode':
-    encrypt(text=text, shift=shift)
-else:
-    decrypt(text=text, shift=shift)
+caesar(text=text, shift=shift, direction=direction)
